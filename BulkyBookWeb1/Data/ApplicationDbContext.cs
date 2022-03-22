1b6d7cc7ac1c1f6a46c95f39e8de2e288d2d7907
EnzoSani <enzosanabria23@gmail.com>
2022-03-22 02:28:20 -0300

    Add ApplicationDbContext

---
+++ BulkyBookWeb1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb1.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        {
+
+        }
+        DbSet<Category> Categories { get; set; }
+    }
+}
