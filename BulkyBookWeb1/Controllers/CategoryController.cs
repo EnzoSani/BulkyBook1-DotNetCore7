7779c830807dbf01a87d467a07c7518848d08af6
EnzoSani <enzosanabria23@gmail.com>
2022-04-04 17:23:21 -0300

    Create Category View

--- BulkyBookWeb1/Controllers/CategoryController.cs
+++ BulkyBookWeb1/Controllers/CategoryController.cs
@@ -17,5 +17,11 @@ namespace BulkyBookWeb1.Controllers
            IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        //Get
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
     }
 }
