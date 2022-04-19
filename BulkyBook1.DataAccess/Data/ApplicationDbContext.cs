67f7e1a50aa2c07be13fb149d404c47438edb5c7
EnzoSani <enzosanabria23@gmail.com>
2022-04-19 19:34:51 -0300

    Create Product Table

--- BulkyBook1.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook1.DataAccess/Data/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace BulkyBook1.DataAccess
         }
        public DbSet<Category> Categories { get; set; }
        public DbSet<CoverType> CoverTypes { get; set; }
+       public DbSet<Product> Products { get; set; }
     }
 }
