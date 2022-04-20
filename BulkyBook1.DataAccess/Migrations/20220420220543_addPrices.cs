1f01b71ae3971c30a6615dda065ea95514ec8c6a
EnzoSani <enzosanabria23@gmail.com>
2022-04-20 19:11:18 -0300

    Create Product Controller

---
+++ BulkyBook1.DataAccess/Migrations/20220420220543_addPrices.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb1.Migrations
+{
+    public partial class addPrices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ListPrice50",
+                table: "Products",
+                newName: "Price50");
+
+            migrationBuilder.RenameColumn(
+                name: "ListPrice100",
+                table: "Products",
+                newName: "Price100");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Products",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "Price50",
+                table: "Products",
+                newName: "ListPrice50");
+
+            migrationBuilder.RenameColumn(
+                name: "Price100",
+                table: "Products",
+                newName: "ListPrice100");
+        }
+    }
+}
---
+++ BulkyBookWeb1/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,103 @@
+﻿using BulkyBook1.DataAccess;
+using BulkyBook1.DataAccess.IRepository;
+using BulkyBook1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;    
+        }
+        public IActionResult Index()
+        {
+           IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
+            return View(objProductList);
+        }
+        //Get
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product obj)
+        {   
+          
+            if (ModelState.IsValid) { 
+            _unitOfWork.Product.Add(obj);
+            _unitOfWork.save();
+                TempData["success"] = "Product created successfully";
+            return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var ProductFromDbFirst = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+
+            if (ProductFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDbFirst);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.save();
+                TempData["success"] = "Product edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var ProductFromDbFirst = _unitOfWork.Product.GetFirstOrDefault(u=>u.Id==id);
+            if (ProductFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDbFirst);
+        }
+        //Post
+        [HttpPost, ActionName ("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                _unitOfWork.Product.Remove(obj);
+            _unitOfWork.save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index");
+            
+        }
+    }
+}
