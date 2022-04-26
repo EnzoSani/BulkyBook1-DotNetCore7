0903fd88a4cd28ddb39bcfeba8998e0d354416a1
EnzoSani <enzosanabria23@gmail.com>
2022-04-26 03:27:40 -0300

    Loading Datatables

---
+++ BulkyBookWeb1/wwwroot/js/product.js
@@ -0,0 +1,18 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#tdlData').DataTable({
+        "ajax": {
+            "url":"/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "title", "with": "15%" },
+            { "data": "isbn", "with": "15%" },
+            { "data": "price", "with": "15%" },
+            { "data": "author", "with": "15%" }
+           
+        ]
+    });
+}
