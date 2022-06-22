3ad631a085b8c1b665b6b2bc2186187b0ac6e6e9
EnzoSani <enzosanabria23@gmail.com>
2022-06-21 21:28:08 -0300

    Price calculation based on quantity

--- BulkyBook1.Models/ShoppingCart.cs
+++ BulkyBook1.Models/ShoppingCart.cs
@@ -21,6 +21,8 @@ namespace BulkyBook1.Models
         public string ApplicationUserId { get; set; }
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
-        public ApplicationUser ApplicationUser { get; set; }    
+        public ApplicationUser ApplicationUser { get; set; }
+        [NotMapped]
+        public double Price { get; set; }
     }
 }
