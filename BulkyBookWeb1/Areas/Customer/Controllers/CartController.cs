e333a0fdde8437037e8c20a492362a5837ce8bc0
EnzoSani <enzosanabria23@gmail.com>
2023-09-07 01:14:16 -0300

    Remove From Session

--- BulkyBookWeb1/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb1/Areas/Customer/Controllers/CartController.cs
@@ -215,6 +215,8 @@ namespace BulkyBookWeb1.Areas.Customer.Controllers
             if (cart.Count <= 1)
             {
                 _unitOfWork.ShoppingCart.Remove(cart);
+                var count = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count - 1;
+                HttpContext.Session.SetInt32(SD.SessionCart, count);
             }
             else
             {
@@ -228,6 +230,8 @@ namespace BulkyBookWeb1.Areas.Customer.Controllers
             var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
             _unitOfWork.ShoppingCart.Remove(cart);
             _unitOfWork.save();
+            var count = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+            HttpContext.Session.SetInt32(SD.SessionCart, count);
             return RedirectToAction(nameof(Index));
         }
 
